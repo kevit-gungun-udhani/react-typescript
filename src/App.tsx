84cb94597c620faf8cab405fa94256dc9e5debd8
kevit-gungun-udhani <gungun.udhani@kevit.io>
2024-08-29 13:41:36 +0530

    feat/ Todo component, data model created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Todos from "./components/Todos";
-
+import Todo from "./models/todo";
 function App() {
+  const myTodos = [new Todo('TS'), new Todo('React-TS')]
+  console.log(myTodos);
+  
   return (
-    <Todos items={['ts', 'React-ts']}>Hello</Todos>
+    <Todos items={myTodos}>Hello</Todos>
   );
 }
 
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Todo from '../models/todo';
 
-const Todos: React.FC<{items: string[], children: string}>= (props) => {
+const Todos: React.FC<{items: Todo[], children: string}>= (props) => {
     return (
         <ul>
-            {props.items.map((item) => <li key={item}>{item}</li>)} 
+            {props.items.map((item) => <li key={item.id}>{item.text}</li>)} 
             {props.children}
         </ul>
     );
